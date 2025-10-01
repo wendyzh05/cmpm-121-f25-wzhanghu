3c4d5193ea9205ae6ab9ed187547e5b8fdacd5a9
Wendy Zhang <156264212+wendyzh05@users.noreply.github.com>
2025-10-01 17:04:43 +0000

    Add counter

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@
 console.log("🎮 CMPM 121 - Starting...");
 
 // Simple counter for demonstration
-// deno-lint-ignore prefer-const
 let counter: number = 0;
 
 // Create basic HTML structure
@@ -22,5 +21,6 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
+  counter += 1;
   console.log("I have these thingies:", button, counterElement, counter);
 });
